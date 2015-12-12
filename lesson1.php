f59087028ae69102585879decc05e15087ba6aa9
kisiloleg1980 <kisiloleg1980@ukr.net>
2015-12-12 10:55:06 +0200

    1

---
+++ lesson1.php
@@ -0,0 +1,21 @@
+<form method="get">
+ <p>
+  <input type="text" name="name" 
+   value="<?=$_REQUEST['name']?>" 
+   placeholder="Ваше имя"> 
+ </p>
+ <p>
+  <input type="text" name="age"
+   value="<?=$_REQUEST['age']?>"
+   placeholder="Ваш возраст">
+ </p> 
+ <p>
+  <input type="submit" value="send">
+ </p>
+</form>
+<?php
+ if ($_REQUEST['age'] > 20) {
+  echo "Меня зовут ",$_REQUEST['name'], "<br>";
+  echo "Мне  ",$_REQUEST['age'], " года<br>";
+ }
+ ?>
